d7c58b468bdb910bbb3efd87cc1dbaae4a9458c9
Rana <rana.potu@gmail.com>
2021-10-16 12:55:04 +0100

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo';
+
+  task!: string;
+
+  todos: string[] = [];
+
+  submit() {
+    console.log(this.task);
+    this.todos.push(this.task);
+    this.task = '';
+  }
+
+  delete(i: number) {
+    this.todos.splice(i, 1);
+  }
+
+  deleteAll() {
+    console.log('Del all clicked');
+
+    this.todos.splice(0, this.todos.length);
+    this.task = '';
+    console.log(this.todos);
+  }
 }
